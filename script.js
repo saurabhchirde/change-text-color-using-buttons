698cf51f44b5a25ddf5945072110e694ad035c74
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-18 15:38:39 +0530

    script: start and btn eventListener

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const btnGreen = document.querySelector("green");
 const btnBlue = document.querySelector(".blue");
 const output = document.querySelector(".output");
 const clear = document.querySelector(".clear");
+
+function start() {
+  input.value = "";
+  output.innerText = "";
+}
+
+btnRed.addEventListener("click", red);
+btnGreen.addEventListener("click", green);
+btnBlue.addEventListener("click", blue);
+
+start();
