6c3daddc5cb045da6a59fd03cafa57a42f756329
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-18 15:55:10 +0530

    script: done with logic

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 const input = document.querySelector(".inputText");
-const btnRed = document.querySelector("red");
-const btnGreen = document.querySelector("green");
+const btnRed = document.querySelector(".red");
+const btnGreen = document.querySelector(".green");
 const btnBlue = document.querySelector(".blue");
 const output = document.querySelector(".output");
-const clear = document.querySelector(".clear");
+const btnClear = document.querySelector(".clear");
 
 function start() {
   input.value = "";
   output.innerText = "";
 }
 
+const red = () => {
+  output.style.color = "red";
+  output.innerText = input.value;
+};
+
+const green = () => {
+  output.style.color = "green";
+  output.innerText = input.value;
+};
+
+const blue = () => {
+  output.style.color = "blue";
+  output.innerText = input.value;
+};
+
 btnRed.addEventListener("click", red);
 btnGreen.addEventListener("click", green);
 btnBlue.addEventListener("click", blue);
+btnClear.addEventListener("click", start);
 
 start();
