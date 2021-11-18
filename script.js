966f3ee38ba881ea4051100824c5c09fa4e38fb8
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-18 15:35:21 +0530

    script: element selection

---
+++ script.js
@@ -0,0 +1,6 @@
+const input = document.querySelector(".inputText");
+const btnRed = document.querySelector("red");
+const btnGreen = document.querySelector("green");
+const btnBlue = document.querySelector(".blue");
+const output = document.querySelector(".output");
+const clear = document.querySelector(".clear");
